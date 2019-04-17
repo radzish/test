32a07aaecd27758b695c7ab120fca6cd8e26b08a
Alexander Radzishevsky <radzish@gmail.com>
2019-04-17 17:41:25 +0300

    test db connection

--- src/index.js
+++ src/index.js
@@ -8,26 +8,28 @@ const connectionString = {
     port: 5432,
 };
 
-console.log(`connecting to DB: ${process.env.DATABASE_USER};${process.env.DATABASE_PASSWORD};${process.env.DATABASE_NAME}`);
+function test() {
+    const pool = new pg.Pool(connectionString);
 
-const pool = new pg.Pool(connectionString);
+    pool.connect(function(err, client, done) {
 
-pool.connect(function(err, client, done) {
+        console.log(`ERROR: ${err}`);
 
-    console.log(`ERROR: ${err}`);
+        const query = client.query(new pg.Query("select * from t_test"))
+        query.on('row', (row) => {
+            console.log(row);
+        });
+        query.on('end', (res) => {
+            // pool shutdown
+            console.log("ending");
+            pool.end()
+        });
+        query.on('error', (res) => {
+            console.log(res);
+        });
 
-    const query = client.query(new pg.Query("select * from t_test"))
-    query.on('row', (row) => {
-        console.log(row);
+        done()
     });
-    query.on('end', (res) => {
-        // pool shutdown
-        console.log("ending");
-        pool.end()
-    });
-    query.on('error', (res) => {
-        console.log(res);
-    });
+}
 
-    done()
-});
+setTimeout(test, 10000);
